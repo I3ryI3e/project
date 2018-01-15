d80c1b70aafcc53d7467ee86882ebc79b4ad21be
I3ryI3e <jgrunho@gmail.com>
2018-01-15 01:49:59 +0000

    Win + Power Up

---
+++ cliente/PowerUp.h
@@ -0,0 +1,14 @@
+#ifndef POWERUP_H
+#define POWERUP_H
+#include <QGraphicsPixmapItem>
+
+class PowerUp: public QGraphicsPixmapItem {
+public:
+    PowerUp();
+    virtual ~PowerUp();
+private:
+
+};
+
+#endif /* POWERUP_H */
+
---
+++ cliente/Win.cpp
@@ -0,0 +1,8 @@
+#include "Win.h"
+
+Win::Win() {
+    widget.setupUi(this);
+}
+
+Win::~Win() {
+}
